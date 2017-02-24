3aabadedd1ff6ed2ba35109a7cd1d61ddd759bc0
d-shah93 <dshah46@uic.edu>
2017-02-24 13:36:40 -0600

    circle + s to take picture

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from tkinter import *
 import cv2
+import time
 
 class App:
+    def get_image(self, camera):
+        retval, im = camera.read()
+        h, w, c = im.shape
+        print("testing")
+        print (h, w, c)
+        return im
 
     def __init__(self, master):
+        vc = cv2.VideoCapture(0)
+        ramp_frames = 30
         frame = Frame(master)
         frame.pack()
-        cv2.namedWindow("Deep is AMAZING",cv2.WND_PROP_FULLSCREEN)
-        vc = cv2.VideoCapture(0)
-        cv2.setWindowProperty("Deep is AMAZING", cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        w = frame.winfo_width()
+        h = frame.winfo_height()
+        cv2.namedWindow("Deep is AMAZING",)
+        #cv2.setWindowProperty("Deep is AMAZING", cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
         if vc.isOpened():
             rval, frame = vc.read()
         else:
@@ -16,22 +26,27 @@ class App:
         while rval:
             cv2.imshow("Deep is AMAZING", frame)
             rval, frame = vc.read()
+            cv2.circle(frame, (320, 400), 25, (169, 169, 169),  3,  -1)
             key = cv2.waitKey(20)
+            if (key == 115) or (key == 87):
+                camera_capture = self.get_image(vc)
+                name = time.strftime("%H%M%S")
+                file = "C:\\Users\deepshah\Documents\GitHub\Photobooth\Pictures\\"  + name + ".png"
+                cv2.imwrite(file, camera_capture)
+                print("File printed to %s", file)
+                #del(vc)
             if key == 27:
                 break
         cv2.destroyWindow("Deep IS AMAZING")
+
 root = Tk()
 app = App(root)
 root.mainloop()
 
-
-
-
-        #w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-#root.overrideredirect(1)
-#root.geometry("%dx%d+0+0" % (w,h))
-#root.configure(background='black')
-#app = App(root)
-
-#root.mainloop()
-#root.destroy()
+# w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+# root.overrideredirect(1)
+# root.geometry("%dx%d+0+0" % (w,h))
+# root.configure(background='black')
+# app = App(root)
+# root.mainloop()
+# root.destroy()
