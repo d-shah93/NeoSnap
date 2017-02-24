7bab5959322ab438b0a83e621e1d01b141b9bfc0
achave42 <achave42@uic.edu>
2017-02-24 15:18:23 -0600

    my main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Allen changes
 from tkinter import *
 import cv2
 
@@ -6,20 +7,20 @@ class App:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
-        cv2.namedWindow("Deep is AMAZING",cv2.WND_PROP_FULLSCREEN)
+        cv2.namedWindow("Allen is AMAZING",cv2.WND_PROP_FULLSCREEN)
         vc = cv2.VideoCapture(0)
-        cv2.setWindowProperty("Deep is AMAZING", cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        cv2.setWindowProperty("Allen is AMAZING", cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
         if vc.isOpened():
             rval, frame = vc.read()
         else:
             rval = False
         while rval:
-            cv2.imshow("Deep is AMAZING", frame)
+            cv2.imshow("Allen is AMAZING", frame)
             rval, frame = vc.read()
             key = cv2.waitKey(20)
             if key == 27:
                 break
-        cv2.destroyWindow("Deep IS AMAZING")
+        cv2.destroyWindow("Allen IS AMAZING")
 root = Tk()
 app = App(root)
 root.mainloop()
