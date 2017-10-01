1e8ffe84e30e02045edbc5c01b00091c53a47090
Deep Shah <dshah46@uic.edu>
2017-10-01 14:20:40 -0500

    step by step tutorial incomplete

--- GUIv1.1/main.py
+++ GUIv1.1/main.py
@@ -102,32 +102,9 @@ class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
         print(self.resx)
         print(self.resy)
 
-    def upload(dbx, fullname, folder, subfolder, name, overwrite=False):
-        """Upload a file.
-        Return the request response, or None in case of error.
-        """
-        path = '/%s/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'), name)
-        while '//' in path:
-            path = path.replace('//', '/')
-        mode = (dropbox.files.WriteMode.overwrite
-                if overwrite
-                else dropbox.files.WriteMode.add)
-        mtime = os.path.getmtime(fullname)
-        with open(fullname, 'rb') as f:
-            data = f.read()
-        with stopwatch('upload %d bytes' % len(data)):
-            try:
-                res = dbx.files_upload(
-                    data, path, mode,
-                    client_modified=datetime.datetime(*time.gmtime(mtime)[:6]),
-                    mute=True)
-            except dropbox.exceptions.ApiError as err:
-                print('*** API error', err)
-                return None
-        print('uploaded as', res.name.encode('utf8'))
-        return res
-
 # I feel better having one of these
+
+
 def main():
     # a new app instance
     app = QApplication(sys.argv)
